5fdc3b121ca761df4f219dff2a619b6373f61ac9
Guillermo Rodríguez <guille.rodriguez@gmail.com>
2020-04-22 19:22:06 +0200

    Updated

--- js/site.js
+++ js/site.js
@@ -6,3 +6,11 @@ $('#select-lang').change(function(ev) {
     $$.setCookie('lang', this.value.substring(0, 2));
     window.location.href = this.value.substring(3);
 });
+
+lightbox.option({
+    'resizeDuration': 100,
+    'imageFadeDuration': 100,
+    'fadeDuration': 100,
+    // For galleries in the references section
+    'albumLabel': document.title + ': %1 / %2'
+})
